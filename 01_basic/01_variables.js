9afa9f25b4cf1993fb795753f8b7d592352736ff
Abhishek Dwivedi <37124925+abhidwi555@users.noreply.github.com>
2023-12-27 22:57:14 +0000

    new

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accID =  456;
+let title = "abhi";
+var email = "lamb.d999@gmail.com"
+
+console.log(title);
+console.log(email);
+console.log(accID);
+
+title="fff";
+
+email = "abhi@gmail.com"
+
+console.table([email , accID , title]);
+
+
