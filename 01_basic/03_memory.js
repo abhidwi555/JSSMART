3ff8b60ddb9cc49e385bf42fcf069da00f1fbddb
Abhishek Dwivedi <37124925+abhidwi555@users.noreply.github.com>
2023-12-28 15:04:44 +0000

    commit done

---
+++ 01_basic/03_memory.js
@@ -0,0 +1,25 @@
+let name1 = "abhishek";
+let name2 =name1;
+
+console.log(name1);
+console.log(name2);
+
+name1="abhi"
+console.log(name2);
+
+
+let user1=
+{
+    email : "abc@gmil.com",
+    name : "Abhishek"
+}
+
+let user2 = user1;
+
+user2.email = "passbook@ybl.com";
+
+console.log(user1);
+console.log(user2);
+
+
+
